1ac6af0848a854affa89c8f9586783066c940d94
Tri <trimpham98@gmail.com>
2023-03-08 13:02:17 -0800

    Add SolveMaze method

--- MazeExample.cpp
+++ MazeExample.cpp
@@ -22,60 +22,82 @@ using namespace std;
 */
 
 
-void SolveMaze(int row, int col, char maze[][100], int rowSz, int colSz, bool& foundEnd, string& finaldir, string currDir) {
+void
+SolveMaze(int row, int col, char maze[][100], int rowSz, int colSz, bool &foundEnd, string currDir, string &finalDir) {
+    if (foundEnd) {
+        return;
+    }
 
+    if (row < 0 || col < 0 || row >= rowSz || col >= colSz) {
+        return;
+    }
+
+    if (maze[row][col] == '.' || maze[row][col] == '@') {
+        return;
+    }
+
+    if (maze[row][col] == 'F') {
+        finalDir = currDir;
+        foundEnd = true;
+        return;
+    }
+
+    maze[row][col] = '.';
+    SolveMaze(row - 1, col, maze, rowSz, colSz, foundEnd, currDir + "U", finalDir); // N
+    SolveMaze(row + 1, col, maze, rowSz, colSz, foundEnd, currDir + "D", finalDir); // S
+    SolveMaze(row, col + 1, maze, rowSz, colSz, foundEnd, currDir + "R", finalDir); // E
+    SolveMaze(row, col - 1, maze, rowSz, colSz, foundEnd, currDir + "L", finalDir); // W
 }
 
-int main()
-{
-	ifstream mazeFile("maze.txt");
-	char maze[100][100]; // make larger than required
+int main() {
+    ifstream mazeFile("maze.txt");
+    char maze[100][100]; // make larger than required
 
-	// Initialize the maze
-	for (int i = 0; i < 100; i++)
-		for (int j = 0; j < 100; j++) {
-			maze[i][j] = ' ';
-		}
+    // Initialize the maze
+    for (int i = 0; i < 100; i++)
+        for (int j = 0; j < 100; j++) {
+            maze[i][j] = ' ';
+        }
 
-	int row = 0;
-	int startRow, startCol;
+    int row = 0;
+    int startRow, startCol;
 
-	// Read the file line by line an initialize the maze
-	string line;
-	while (getline(mazeFile, line)) {
-		cout << line << endl;
-		for (int i = 0; i < (int) line.length(); i++) {
-			maze[row][i] = line[i];
-			if (maze[row][i] == 'S') {
-				startRow = row;
-				startCol = i;
-				maze[row][i] = ' ';
-			}
-		}
-		row++;
-	}
+    // Read the file line by line an initialize the maze
+    string line;
+    while (getline(mazeFile, line)) {
+        cout << line << endl;
+        for (int i = 0; i < (int) line.length(); i++) {
+            maze[row][i] = line[i];
+            if (maze[row][i] == 'S') {
+                startRow = row;
+                startCol = i;
+                maze[row][i] = ' ';
+            }
+        }
+        row++;
+    }
 
-	cout << "Start Position: (" << startRow << "," << startCol << ")" << endl;
+    cout << "Start Position: (" << startRow << "," << startCol << ")" << endl;
 
-	// figure out the number of rows and columns
-	int rowSz = row;
-	int colSz = line.length();
-	
-	bool foundEnd = false;
+    // figure out the number of rows and columns
+    int rowSz = row;
+    int colSz = line.length();
 
-	string finaldir = "";
-	string currDir = "";
-	SolveMaze(startRow, startCol, maze, rowSz, colSz, foundEnd, finaldir, currDir);
-	
-	cout << "Directions: " << finaldir << endl;
+    bool foundEnd = false;
 
-	// Print out the maze with the path shown
-	for (int i = 0; i < rowSz; i++) {
-		for (int j = 0; j < colSz; j++) {
-			cout << maze[i][j];
-		}
-		cout << endl;
-	}
-	
+    string finaldir = "";
+    string currDir = "";
+    SolveMaze(startRow, startCol, maze, rowSz, colSz, foundEnd, currDir, finaldir);
+
+    cout << "Directions: " << finaldir << endl;
+
+    // Print out the maze with the path shown
+    for (int i = 0; i < rowSz; i++) {
+        for (int j = 0; j < colSz; j++) {
+            cout << maze[i][j];
+        }
+        cout << endl;
+    }
+
     return 0;
 }
